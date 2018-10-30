351fe6b77d360e2b325f2990f8699ab451260848
Joshua Waring <Joshhua123@gmail.com>
2018-10-29 21:57:55 -0400

    Initial commit

---
+++ Assets/SSAO/SSAO.shader
@@ -0,0 +1,48 @@
+﻿Shader "Hidden/SSAO"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    { 
+        Tags { "RenderType"="Opaque"}
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "SSAO.cginc"
+
+            sampler2D _MainTex;
+            sampler2D _CameraDepthTexture; 
+
+            fixed4 frag (v2f i) : SV_Target
+            { 
+                float depth = tex2D(_CameraDepthTexture, i.uv);
+                //depth = LinearEyeDepth(depth);
+
+                // Calculate the world position of this pixel   
+                float2 uvClip = float2(i.uv * 2.0 - 1.0);
+                float4 clipPos = float4(uvClip, depth, 1.0);
+                float4 viewPos = mul(projToView, clipPos);
+  
+                float3 worldSpace = mul(viewToWorld, viewPos.xyz / viewPos.w).xyz;
+    
+                // use a set of offset vectors to sample from the surrounding world
+
+                
+
+                return fixed4(worldSpace, 1);
+ 
+
+
+                //return fixed4(depth, depth, depth, 1);
+                //UNITY_OUTPUT_DEPTH(i.uv);
+                //return fixed4(COMPUTE_EYEDEPTH(i.depth));
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/SSAO/SSAO_PostProcessing.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine; 
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(SSAO_Renderer), PostProcessEvent.AfterStack, "Custom/Screen Space Ambient Occlusion")]
+public sealed class SSAO_PostProcessing : PostProcessEffectSettings
+{ 
+}
+
+public class SSAO_Renderer : PostProcessEffectRenderer<SSAO_PostProcessing>
+{
+
+    private static readonly int DownRenderTargetID = Shader.PropertyToID("LBAO_Downsample");
+    private static readonly int BlurRenderTargetID = Shader.PropertyToID("LBAO_Blur");
+ 
+    private Material _material;
+      
+    public override void Init()
+    {
+        var shader = Shader.Find("Hidden/SSAO");
+        _material = new Material(shader); 
+         
+
+        UpdateMaterialProperties();
+        base.Init();
+    } 
+
+    public void UpdateMaterialProperties()
+    { 
+        /*
+        _material.SetFloat("_Threshold", settings.threshold);
+        _material.SetInt("_Samples", settings.samples);
+        _material.SetFloat("_Radius", settings.radius);
+        _material.SetFloat("_Intensity", settings.intensity);
+        _material.SetFloat("_LumaProtect", 1f - settings.lumaProtect);
+        _material.SetFloat("_BlurSpread", Mathf.Max(settings.radius / 16f, 1f));
+        _material.SetVector("_Direction", settings.direction.value.normalized);
+
+        if (settings.blur)
+            _material.EnableKeyword(SKW_BLUR); 
+        else
+            _material.DisableKeyword(SKW_BLUR); 
+        */
+    }
+
+    public override void Release()
+    {
+        base.Release();
+    }
+
+    public override void Render(PostProcessRenderContext ctx)
+    {
+        //UpdateMaterialProperties();
+        var cmd = ctx.command;  
+
+        cmd.BeginSample("Custom SSAO"); 
+  
+        // https://forum.unity.com/threads/solved-clip-space-to-world-space-in-a-vertex-shader.531492/
+        // We use chriscummings solution for depth to worldspace
+        var view = GL.GetGPUProjectionMatrix(ctx.camera.projectionMatrix, false).inverse; 
+        var viewToWorld = ctx.camera.cameraToWorldMatrix;
+
+        _material.SetMatrix("projToView", view);
+        _material.SetMatrix("viewToWorld", viewToWorld);
+   
+        cmd.Blit(ctx.source, ctx.destination, _material);
+  
+        cmd.EndSample("Custom SSAO");
+          
+    }
+}
