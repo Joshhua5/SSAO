97feac7ff4c11e5d15668205141f4170220c2ac4
Joshua Waring <Joshhua123@gmail.com>
2018-11-08 23:45:26 -0500

    Added uniform random unit vectors

--- Assets/SSAO/SSAO_PostProcessing.cs
+++ Assets/SSAO/SSAO_PostProcessing.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.Experimental.Rendering;
+using System.Collections.Generic;
 
 [Serializable]
 [PostProcess(typeof(SSAO_Renderer), PostProcessEvent.AfterStack, "Custom/Screen Space Ambient Occlusion")]
@@ -14,11 +15,13 @@ public sealed class SSAO_PostProcessing : PostProcessEffectSettings
     [Range(0.5f, 10)]
     public FloatParameter Intensity = new FloatParameter { value = 0.6f };
      
-    [Range(1, 64)]
+    [Range(1, 32)]
     public IntParameter Samples = new IntParameter { value = 32 };
 
     [Range(1, 10)]
-    public IntParameter DropOff = new IntParameter { value = 3 }; 
+    public IntParameter DropOff = new IntParameter { value = 3 };
+
+    public BoolParameter OcclusionOnly = new BoolParameter { value = false };
 }
 
 [ExecuteInEditMode]
@@ -43,11 +46,13 @@ public class SSAO_Renderer : PostProcessEffectRenderer<SSAO_PostProcessing>
     private static readonly int _OcclusionTex = Shader.PropertyToID("_OcclusionTex");
     private static readonly int _Blur = Shader.PropertyToID("_Blur");
     private static readonly int _Occlusion = Shader.PropertyToID("_Occlusion");
-    private static readonly int _MainTex = Shader.PropertyToID("_MainTex");
+    private static readonly int _MainTex = Shader.PropertyToID("_MainTex"); 
+    private static readonly int _RandomSamples = Shader.PropertyToID("_RandomSamples");
 
     private Material _material;
-    private Texture2D _randomTexture;
-    private Texture2D _occlusionFunction;
+    private Texture2D _randomTexture; 
+    private List<Vector4> _randomSamples = new List<Vector4>(64);
+    private System.Random random = new System.Random();
 
     static RenderTextureDescriptor occlusionDescriptor = new RenderTextureDescriptor
     { 
@@ -55,59 +60,74 @@ public class SSAO_Renderer : PostProcessEffectRenderer<SSAO_PostProcessing>
         dimension = UnityEngine.Rendering.TextureDimension.Tex2D,
         colorFormat = RenderTextureFormat.RHalf
     };
+
+    private void UniformRandomOnSphere(out float x, out float y, out float z)
+    {
+        // http://corysimon.github.io/articles/uniformdistn-on-sphere/
+        // These random values aren't uniform, but we can get that by rejecting values that fall outside of a sphere within our cube of random values [-1, 1]^3  
+
+        x = (float)random.NextDouble() * 2f - 1f;
+        y = (float)random.NextDouble() * 2f - 1f;
+        z = (float)random.NextDouble() * 2f - 1f;
+
+        // http://corysimon.github.io/articles/uniformdistn-on-sphere/
+        // These random values aren't uniform, but we can get that by rejecting values that fall outside of a sphere within our cube of random values [-1, 1]^3 
+        float magnitude = Mathf.Sqrt((x * x) + (y * y) + (z * z));
+        if (magnitude >= 1) 
+            UniformRandomOnSphere(out x, out y, out z);  
+        else
+        { 
+            // Normalize
+            x /= magnitude;
+            y /= magnitude;
+            z /= magnitude;
+        }
+    }
      
     public override void Init()
     {
+        // Setup the shader
         var shader = Shader.Find("Hidden/SSAO");
         _material = new Material(shader);
-        var random = new System.Random();
-         
-        _randomTexture = new Texture2D(256, 256, TextureFormat.RGBAHalf, false, true);
-        for (int x = 0; x < 256; ++x)
+
+        // Generate the random texture to act as a seed for each pixel
+        const int randomWidth = 256, randomHeight = 256;
+        _randomTexture = new Texture2D(randomWidth, randomHeight, TextureFormat.RGBAHalf, false, true);
+        var scratchColor = new Color(0, 0, 0, 1);
+        for (int x = 0; x < randomWidth; ++x)
         {
-            var rand = new Vector3();
-            for (int y = 0; y < 256; ++y)
+            for (int y = 0; y < randomHeight; ++y)
             {
-                rand.Set(
-                    (float)random.NextDouble() * 2f - 1f,
-                    (float)random.NextDouble()* 2f - 1f,
-                    (float)random.NextDouble() * 2f - 1f);
-
-                rand.Normalize();
-
-                _randomTexture.SetPixel(x, y, new Color
-                {
-                    r = rand.x,
-                    g = rand.y,
-                    b = rand.z,
-                    a = 1
-                });
+                UniformRandomOnSphere(out scratchColor.r, out scratchColor.g, out scratchColor.b);
+                _randomTexture.SetPixel(x, y, scratchColor);
             }
         }
         _randomTexture.Apply();
 
-        _occlusionFunction = new Texture2D(32, 1, TextureFormat.RHalf, false, true);
-        _occlusionFunction.SetPixel(0, 0, Color.black);
-        for(int x = 0; x < 31; ++x)
-        {
-            // Linear function
-            _occlusionFunction.SetPixel(1 + x, 0, new Color(1f - (x / 31f), 0, 0));
-        }
-        _occlusionFunction.Apply();
+        // Generate the random values that will be used for each sample, by doting them with the random 'seed'
         
+        for(int i = 0; i < 32; ++i)
+        {
+            float x, y, z;
+            UniformRandomOnSphere(out x, out y, out z); 
+            _randomSamples.Add(new Vector4(x, y, z, 1)); 
+        }
+
+         _material.SetVectorArray(_RandomSamples, _randomSamples);
+          
         UpdateMaterialProperties();
         base.Init();
     } 
 
     public void UpdateMaterialProperties()
-    {
-        _material.SetInt(_Samples, settings.Samples.value);
+    { 
+        _material.SetInt(_Samples, settings.Samples.value - settings.Samples.value % 3);
         _material.SetFloat(_DropOffFactor, settings.DropOff.value);
         _material.SetFloat(_Intensity, settings.Intensity.value);
         _material.SetFloat(_Range, settings.Range.value);
-         
-        _material.SetTexture(_RandomTex, _randomTexture);
-        _material.SetTexture(_OcclusionFunction, _occlusionFunction);
+
+        _material.SetVectorArray(_RandomSamples, _randomSamples);
+        _material.SetTexture(_RandomTex, _randomTexture); 
     }
 
     public override void Release()
@@ -132,35 +152,43 @@ public class SSAO_Renderer : PostProcessEffectRenderer<SSAO_PostProcessing>
         _material.SetMatrix(_ClipToView, viewToClip.inverse);
         _material.SetMatrix(_ViewToWorld, viewToWorld);
         _material.SetMatrix(_WorldToView, worldToView);
-        
-        occlusionDescriptor.width = ctx.screenWidth;
-        occlusionDescriptor.height = ctx.screenHeight;
 
-        cmd.GetTemporaryRT(_Occlusion, occlusionDescriptor);
-        cmd.GetTemporaryRT(_Blur, occlusionDescriptor);
+        if (settings.OcclusionOnly)
+        { 
+            cmd.Blit(ctx.source, ctx.destination, _material, 0); 
+        }
+        else
+        {  
+            occlusionDescriptor.width = ctx.screenWidth;
+            occlusionDescriptor.height = ctx.screenHeight;
 
-        // Occlusion
-        cmd.BeginSample("Occlusion");
-        cmd.Blit(ctx.source, _Occlusion, _material, 0);
-        cmd.EndSample("Occlusion");
+            // Occlusion
+            cmd.GetTemporaryRT(_Occlusion, occlusionDescriptor);
 
-        cmd.BeginSample("Blur");
-        // Horizontal Blur
-        _material.SetVector(_BlurOffset, new Vector2(1.3333333f / ctx.camera.pixelWidth, 0));
-        cmd.SetGlobalTexture(_OcclusionTex, _Occlusion);
-        cmd.Blit(_Occlusion, _Blur, _material, 1);
-          
-        cmd.ReleaseTemporaryRT(_Occlusion); 
-        // Vertical Blur
-        _material.SetVector(_BlurOffset, new Vector2(0, 1.3333333f / ctx.camera.pixelHeight)); ;
-        cmd.SetGlobalTexture(_MainTex, ctx.source);
-        cmd.SetGlobalTexture(_OcclusionTex, _Blur);
-        cmd.Blit(ctx.source, ctx.destination, _material, 2);
+            cmd.BeginSample("Occlusion");
+            cmd.Blit(ctx.source, _Occlusion, _material, 0);
+            cmd.EndSample("Occlusion");
 
-        cmd.ReleaseTemporaryRT(_Blur);
-        cmd.EndSample("Blur");
+            // Horizontal Blur
+            cmd.GetTemporaryRT(_Blur, occlusionDescriptor);
 
+            cmd.BeginSample("Blur");
+            _material.SetVector(_BlurOffset, new Vector2(1.3333333f / ctx.camera.pixelWidth, 0));
+            cmd.SetGlobalTexture(_OcclusionTex, _Occlusion);
+            cmd.Blit(_Occlusion, _Blur, _material, 1);
 
+            cmd.ReleaseTemporaryRT(_Occlusion);
+
+            // Vertical Blur
+            _material.SetVector(_BlurOffset, new Vector2(0, 1.3333333f / ctx.camera.pixelHeight));
+            cmd.SetGlobalTexture(_MainTex, ctx.source);
+            cmd.SetGlobalTexture(_OcclusionTex, _Blur);
+            cmd.Blit(ctx.source, ctx.destination, _material, 2);
+
+            cmd.ReleaseTemporaryRT(_Blur);
+
+            cmd.EndSample("Blur");
+        }
         cmd.EndSample("Custom SSAO"); 
     }
 }
